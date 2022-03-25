b47d5cabfc0f47b26e9f61f863713b8771cdbbd9
Pawandeep1 <97855691+Pawandeep1@users.noreply.github.com>
2022-03-25 18:33:08 +0530

    lab2 and lab3 final

--- Net_Information_L3.ps1
+++ Net_Information_L3.ps1
@@ -1,2 +1,3 @@
-﻿Get-Ciminstance -ClassName Win32_networkadapterconfiguration | Where-Object { $_.ipenabled -eq "true"} | ft -AutoSize Description, Index, IPAddress, IPSubnet, DNSDomain,
-DNSServerSearchOrder
+﻿Get-Ciminstance -ClassName Win32_networkadapterconfiguration | Where-Object { $_.ipenabled -eq "true"} |
+ ft -AutoSize Description, Index, IPAddress, IPSubnet, DNSDomain,
+ DNSServerSearchOrder
