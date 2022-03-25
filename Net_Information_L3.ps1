c51ce81689954f5122ed073e8406b77079f10f5c
Pawandeep1 <97855691+Pawandeep1@users.noreply.github.com>
2022-03-25 18:26:14 +0530

    powershell lab3

---
+++ Net_Information_L3.ps1
@@ -0,0 +1,2 @@
+﻿Get-Ciminstance -ClassName Win32_networkadapterconfiguration | Where-Object { $_.ipenabled -eq "true"} | ft -AutoSize Description, Index, IPAddress, IPSubnet, DNSDomain,
+DNSServerSearchOrder
