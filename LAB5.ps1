ba1541cb533f50b2139f96768afcd35a79a1ce61
Pawandeep1 <97855691+Pawandeep1@users.noreply.github.com>
2022-04-12 17:46:10 +0530

    lab 5

---
+++ LAB5.ps1
@@ -0,0 +1,21 @@
+﻿Param ([switch]$System, 
+       [switch]$Disks, [switch]$Network)
+
+import-module pawandeep
+
+if ($System -eq $false -and $Disks -eq $false -and $Network -eq $false) {
+	echo "Uses functions in previous labs as a module for this perticular lab."
+	echo "Add Param arguments to get more specific results."
+     pawandeep-System;
+     pawandeep-Disks;pawandeep-Network;
+} else{
+     if ($System) {
+         pawandeep-System;
+     }
+     if ($Disks) {
+         pawandeep-Disks;
+     }
+     if ($Network) {
+         pawandeep-Network;
+     }
+}
